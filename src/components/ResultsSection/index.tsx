1b010c1aa8dd15d86613fe600ea5ee1f4aa1645e
AL424 <algr5113@gmail.com>
2023-10-27 15:20:28 +0300

    feat: display film cards in result section

--- src/components/ResultsSection/index.tsx
+++ src/components/ResultsSection/index.tsx
@@ -1,14 +1,40 @@
 import { Component, ReactNode } from 'react';
+import { getResults } from '../../services/kpApi';
+import { FilmInfo } from '../../types/kp';
+import { FilmCard } from '../FilmCard';
 
-interface MyProps extends React.HTMLAttributes<HTMLElement> {
+interface MyProps {
   searchName?: string;
 }
 
-class ResultSection extends Component<MyProps> {
+interface MyState {
+  filmsInfo: FilmInfo[];
+}
+
+class ResultSection extends Component<MyProps, MyState> {
+  state = {
+    filmsInfo: [],
+  };
+
+  componentDidMount = async () => {
+    const data = await getResults(this.props.searchName);
+    this.setState({ filmsInfo: data });
+  };
+
+  componentDidUpdate = async (prevProps: Readonly<MyProps>) => {
+    if (prevProps.searchName !== this.props.searchName) {
+      const data = await getResults(this.props.searchName);
+      this.setState({ filmsInfo: data });
+    }
+  };
+
   render(): ReactNode {
     return (
       <section>
         <h2>{this.props.searchName || 'запрос не введен'}</h2>
+        {this.state.filmsInfo.map((film) => {
+          return <FilmCard film={film} key={film.id} />;
+        })}
       </section>
     );
   }
