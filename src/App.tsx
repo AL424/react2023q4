073e847d12cd55c895f8fb565e23554a663ede08
AL424 <algr5113@gmail.com>
2023-10-28 15:45:57 +0300

    feat: add button throw error, use ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Component, ReactNode } from 'react';
 import './App.scss';
 import { SearchWrap } from './components/SearchWrap';
+import { ErrorButton } from './components/ErrorButton';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 class App extends Component {
   render(): ReactNode {
-    return <SearchWrap />;
+    return (
+      <ErrorBoundary>
+        <SearchWrap />
+        <ErrorButton />
+      </ErrorBoundary>
+    );
   }
 }
 
